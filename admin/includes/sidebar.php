8694257faadfef2cc8faa4f999006182702aeef8
[rohitpaldeveloper] <[iambittupal@icloud.com]>
2023-04-25 17:49:07 +0530

    tms

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,52 @@
+<ul class="navbar-nav sidebar sidebar-light accordion" id="accordionSidebar">
+  <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+    <div class="sidebar-brand-icon">
+      <!-- <img src="img/logo/travel.png"> -->
+    </div>
+    <div class="sidebar-brand-text mx-3">Travel CodePanda</div>
+  </a>
+  <hr class="sidebar-divider my-0">
+  <li class="nav-item active">
+    <a class="nav-link" href="dashboard.php">
+      <i class="fas fa-fw fa-tachometer-alt"></i>
+      <span>Dashboard</span></a>
+    </li>
+    <hr class="sidebar-divider">
+    <div class="sidebar-heading">Features</div>
+  <li class="nav-item">
+    <a class="nav-link" href="create_package.php">
+      <i class="fab fa-fw fa fa-plus"></i>
+      <span>Create Package</span>
+    </a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="manage_package.php">
+      <i class="fab fa-fw fa-wpforms"></i>
+      <span>Manage Package</span>
+    </a>
+  </li>
+   <li class="nav-item">
+    <a class="nav-link" href="manage_bookings.php">
+      <i class="fas fa-fw fa-chart-area"></i>
+      <span>Bookings</span>
+    </a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="user_register.php">
+      <i class="fas fa-fw fa fa-user"></i>
+      <span>Register User</span>
+    </a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="manage_users.php">
+      <i class="fas fa-fw fa fa-users"></i>
+      <span>Manage Users</span>
+    </a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="permissions.php">
+      <i class="fab fa-fw fa fa-cogs"></i>
+      <span>Set Permission</span>
+    </a>
+  </li>
+</ul>
